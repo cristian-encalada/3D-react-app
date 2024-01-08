f3bc5895ec1355886cabce674b299db26ec634da
Cristian Encalada <77868316+cristian-encalada@users.noreply.github.com>
2024-01-08 20:42:00 -0300

    First commit. Add all base files

---
+++ src/components/index.js
@@ -0,0 +1,13 @@
+import CTA from "./CTA";
+import Loader from "./Loader";
+import Navbar from "./Navbar";
+import HomeInfo from "./HomeInfo";
+import Alert from "./Alert";
+
+export {
+    Alert,
+    Loader,
+    Navbar,
+    HomeInfo,
+    CTA,
+}
