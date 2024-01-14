93e24ae86e0cbcec0de6522d1f40b90902c001b1
Cristian Encalada <77868316+cristian-encalada@users.noreply.github.com>
2024-01-14 14:57:40 -0300

    Update Sky 3D model

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,7 +4,7 @@ import { Suspense, useEffect, useRef, useState } from "react";
 import sakura from "../assets/sakura.mp3";
 import { HomeInfo, Loader } from "../components";
 import { soundoff, soundon } from "../assets/icons";
-import { Bird, PrincePlanet, Plane } from "../models";
+import { Sky, PrincePlanet, Plane } from "../models";
 
 const Home = () => {
   const audioRef = useRef(new Audio(sakura));
@@ -85,8 +85,8 @@ const Home = () => {
             intensity={1}
           />
 
-          <Bird />
-          {/* <Sky isRotating={isRotating} /> */}
+          {/* <Bird /> */}
+          { <Sky isRotating={isRotating} /> }
           <PrincePlanet
             isRotating={isRotating}
             setIsRotating={setIsRotating}
